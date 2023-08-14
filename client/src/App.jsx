bcc1311943f6c65ebb909cfc025444e101d18b7b
Fadhil <fadhilahdwi.a@gmail.com>
2023-08-15 05:56:10 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from './pages/Register'
+import Chat from './pages/Chat'
+import Login from './pages/Login'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
